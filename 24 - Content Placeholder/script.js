ca208c47b82dc148e96adf07dcc878073ce1ce40
Matt Stein <matt.m.stein@gmail.com>
2024-11-29 18:01:33 -0500

    Adds 3 new projects
    I learnt some more about web development and worked on some more projects.

---
+++ 24 - Content Placeholder/script.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profileImg = document.getElementById('profile-img');
+const nameData = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animatedBgs = document.querySelectorAll('.animated-bg');
+const animatedBgTexts = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(getData, 2500);
+
+function getData() {
+    header.innerHTML = `
+        <img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyYhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />
+    `
+    title.innerHTML = `Lorem ipsum dolor sit amet`;
+    excerpt.innerHTML = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, error.`;
+    profileImg.innerHTML = `<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />`;
+    nameData.innerHTML = `John Doe`;
+    title.innerHTML = `Oct 08, 2020`;
+
+    animatedBgs.forEach(bg => bg.classList.remove('animated-bg'));
+    animatedBgTexts.forEach(bg => bg.classList.remove('animated-bg-text'));
+}
